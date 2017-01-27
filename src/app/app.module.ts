74c0d1ba4681408649a81053b261c7d99c9318bd
Anny <quratulann_iqbal@hotmail.com>
2017-01-27 14:34:39 +0500

    LogIn implemented with angularFire2 authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+// import { NgReduxModule } from 'ng2-redux';
+import 'hammerjs';
 
+// Components
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import 'hammerjs';
+
+export const myFirebaseConfig = {
+    apiKey: 'AIzaSyDprS0fjxKqaIWjFwYuTNP844rkWxnjt1k',
+    authDomain: 'l1opbs-4128f.firebaseapp.com',
+    databaseURL: 'https://l1opbs-4128f.firebaseio.com',
+    storageBucket: 'l1opbs-4128f.appspot.com',
+    messagingSenderId: '514152719484'
+};
+
+export const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
+
+// const routes: Routes =[
+//   { path: '', pathMatch: 'full', redirectTo: 'home' },
+//   { path: 'home', component: HomeComponent },
+//   { path: 'dashboard', component: UserComponent },
+//   { path: 'card_travel', component: CompanyComponent },
+//   { path: 'perm_identity', component: StudentComponent },
+//   { path: 'account_circle', component: AdminComponent },
+//   { path: 'notifications', component: NotificationsComponent },
+//   { path: '**', redirectTo: 'home'}
+
+// ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    // StoreModule,
+    // NgReduxModule,
+    MaterialModule.forRoot(),
+    // RouterModule.forRoot(routes),
+    AngularFireModule.initializeApp(myFirebaseConfig, myFirebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-// import { AngularFire } from 'angularfire2';
-// import { AngularFireModule, AuthProviders, AuthMethods, FirebaseAuthState } from 'angularfire2';
+import { AngularFire, AngularFireModule, AuthProviders, AuthMethods, FirebaseAuthState } from 'angularfire2';
 // import { select } from 'ng2-redux';
 // import { MyActions } from './../store/actions';
-import { Router } from '@angular/router';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -14,22 +13,22 @@ export class SigninComponent {
 
   index: number;
   constructor(
-      private af: AngularFire,
-      private route: Router,
-      private a: MyActions
+      private af: AngularFire
+      // private route: Router,
+      // private a: MyActions
     ) { }
 
   onSignIn(value) {
     // 'signin' action dispatched from redux
-    this.a.signIn(value.eml);
+    // this.a.signIn(value.eml);
 
-        // this.af.auth.login(); // Google login
+        this.af.auth.login(); // Google login
         this.af.auth.login(
           {email: value.eml , password: value.pass},
           {provider: AuthProviders.Password, method: AuthMethods.Password}
         ).then((res) => {
             alert('Sign In Successful!');
-            this.route.navigate(['perm_identity']);
+            // this.route.navigate(['perm_identity']);
         }, (err) => {
             alert(err);
         });
